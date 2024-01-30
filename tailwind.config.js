c575b14f553178d054a28abbaff11537bfcc1a63
Nate J.M <nate@natejonah.com>
2024-01-30 22:11:27 +0000

    Install autoprefixer and postcss and set up configs

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/components/*.jsx', './src/*.jsx'],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+

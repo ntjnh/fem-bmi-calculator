927e1635ceb1eac620acfbe392896d878d5cf652
Nate J.M <nate@natejonah.com>
2024-01-30 22:12:33 +0000

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1 className="font-semibold text-5xl">Body Mass Index Calculator</h1>
+            <p>Better understand your weight in relation to your height using our body mass index (BMI) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and wellbeing.</p>
+        </header>
+    )
+}

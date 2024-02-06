dc3de3a951cc162b914a50f4892539096b39a9bb
Nate J.M <nate@natejonah.com>
2024-02-06 22:54:49 +0000

    create main component and add meaning to it

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import Meaning from "./Meaning";
+
+export default function Main() {
+    return (
+        <main>
+            <div className="container mx-auto">
+                
+                <Meaning />
+
+            </div>
+        </main>
+    )
+}

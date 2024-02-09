a471e74696c10e0d4adc17acf6f118ad16dfa6f0
Nate J.M <nate@natejonah.com>
2024-02-09 23:27:27 +0000

    display limitations in 1 col on mobile

--- src/components/Limitations.jsx
+++ src/components/Limitations.jsx
@@ -12,7 +12,7 @@ const limitations = data.map((item, i) => {
 export default function Limitations() {
     return (
         <section className="mb-16 pt-16">
-            <div className="grid grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                 <div className="bg-cyan-50 p-8 rounded-xl">
                     <h2 className="font-medium leading-[1.125] mb-4 text-4xl tracking-tighter">Limitations of BMI</h2>
                     <p>Although BMI is often a practical indicator of healthy weight, it is not suited for every person. Specific groups should carefully consider their BMI outcomes, and in certain cases, the measurement may not be beneficial to use.</p>

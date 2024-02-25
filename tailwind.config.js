591af691b8f3979a477646d8ba630183140ab11c
Nate J.M <nate@natejonah.com>
2024-02-25 14:53:14 +0000

    Add custom screen widths and center all containers

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 export default {
   content: ['./src/components/*.jsx', './src/*.jsx'],
   theme: {
+    container: {
+      center: true
+    },
     fontFamily: {
       sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif']
+    },
+    screens: {
+      sm: '580px',
+      md: '700px',
+      lg: '930px',
+      xl: '1160px',
+      '2xl': '1500px',
     },
     extend: {
       colors: {

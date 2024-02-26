ab6e0479c9348ba06b1aafd1aca7d90c408eba56
Nate J.M <nate@natejonah.com>
2024-02-26 14:19:12 +0000

    Add container to Limitations and tweak colour

--- src/components/Limitations.jsx
+++ src/components/Limitations.jsx
@@ -14,14 +14,16 @@ const limitations = data.map((item, i) => {
 
 export default function Limitations() {
     return (
-        <section className="mb-24 md:mb-16 px-6 md:px-0 md:pt-16">
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8">
-                <div className="md:bg-cyan-50 mb-9 md:mb-0 md:p-8 rounded-xl text-center md:text-left">
-                    <h2 className="font-medium leading-[1.125] mb-8 md:mb-4 text-[34px] md:text-4xl tracking-tighter">Limitations of BMI</h2>
-                    <p className="text-slate-550">Although BMI is often a practical indicator of healthy weight, it is not suited for every person. Specific groups should carefully consider their BMI outcomes, and in certain cases, the measurement may not be beneficial to use.</p>
-                </div>
+        <section className="mb-24 md:mb-0 px-6 md:px-0 md:py-28 bg-gradient-to-r from-cyan-50 to-white">
+            <div className="container">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8">
+                    <div className="md:bg-[#e4fcfe] mb-9 md:mb-0 md:p-8 rounded-xl text-center md:text-left">
+                        <h2 className="font-medium leading-[1.125] mb-8 md:mb-4 text-[34px] md:text-4xl tracking-tighter">Limitations of BMI</h2>
+                        <p className="text-slate-550">Although BMI is often a practical indicator of healthy weight, it is not suited for every person. Specific groups should carefully consider their BMI outcomes, and in certain cases, the measurement may not be beneficial to use.</p>
+                    </div>
 
-                {limitations}
+                    {limitations}
+                </div>
             </div>
         </section>
     )

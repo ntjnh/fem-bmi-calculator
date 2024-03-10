407311b415a2c2ae9ec2eab009fc080d57a9517c
Nate J.M <nate@natejonah.com>
2024-03-10 14:59:57 +0000

    Build units switching functionality and update calculations

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,40 +11,123 @@ export default function Form() {
         </div>
     )
 
-    const [units, setUnits] = useState('metric')
-    // let heightUnit = 'cm'
-    // let weightUnit = 'kg'
-    const [bmiResult, setBmiResult] = useState(0)
+    const [showImperial, setShowImperial] = useState(false)
+    const [bmiResult, setBmiResult] = useState('')
     const [height, setHeight] = useState('')
     const [weight, setWeight] = useState('')
-
-    let showMetric = true
-    let showImperial = false
-
-    const metricClass = showMetric ? 'block' : 'hidden'
-    const imperialClass = showImperial ? 'block' : 'hidden'
+    const [heightFt, setHeightFt] = useState('')
+    const [heightIn, setHeightIn] = useState('')
+    const [weightSt, setWeightSt] = useState('')
+    const [weightLbs, setWeightLbs] = useState('')
 
     function handleUnitChange(e) {
-        setUnits(e.target.value)
-
-        showMetric = !showMetric
-        showImperial = !showImperial
-
-        // if (units === 'imperial') {
-        //     heightUnit = 'ft'
-        //     weightUnit = 'lbs'
-        // } else {
-        //     heightUnit = 'cm'
-        //     weightUnit = 'kg'
-        // }
+        setShowImperial(prev => !prev)
     }
 
     function handleSubmit(e) {
         e.preventDefault()
 
-        const bmi = calculation(units, height, weight)
-        setBmiResult(bmi)
+        if (showImperial) {
+            const heightI = [Number(heightFt), Number(heightIn)]
+            const weightI = [Number(weightSt), Number(weightLbs)]
+
+            setBmiResult(calculation(showImperial, Number(heightI), Number(weightI)))
+        } else {
+            setBmiResult(calculation(showImperial, Number(height), Number(weight)))
+        }
     }
+
+    const metricFields = (
+        <div className={`flex flex-wrap justify-between pt-6`}>
+            <div className="inline-block mb-11 md:mb-0 relative w-full md:w-[48%]">
+                <label className="absolute -top-7 md:-top-8 text-slate-550 text-sm md:text-base" htmlFor="height">Height</label>
+                <input 
+                    className="border border-[#e0e1e3] p-[22px] md:p-6 rounded-xl font-semibold leading-none text-[22px] w-full placeholder:text-[#C6C6C8]" 
+                    type="number" 
+                    name="height" 
+                    id="height" 
+                    value={height}
+                    onChange={e => setHeight(e.target.value)} 
+                    placeholder="188"
+                />
+                <span className="absolute align-middle font-semibold inline-block leading-none right-6 text-blue-500 text-[23px] top-6 unit-height">cm</span>
+            </div>
+
+            <div className="inline-block relative w-full md:w-[48%]">
+                <label className="absolute -top-7 md:-top-8 text-slate-550 text-sm md:text-base" htmlFor="weight">Weight</label>
+                <input 
+                    className="border border-[#e0e1e3] p-[22px] md:p-6 rounded-xl font-semibold leading-none text-[22px] w-full placeholder:text-[#C6C6C8]" 
+                    type="number" 
+                    name="weight" 
+                    id="weight" 
+                    value={weight}
+                    onChange={e => setWeight(e.target.value)} 
+                    placeholder="80.3" 
+                />
+                <span className="absolute align-middle font-semibold inline-block leading-none right-6 text-blue-500 text-[23px] top-6 unit-weight">kg</span>
+            </div>
+        </div>
+    )
+
+    const imperialFields = (
+        <div className={`flex flex-wrap justify-between pt-6`}>
+            <div className="inline-block mb-11 md:mb-0 relative w-full md:w-[24%]">
+                <label className="absolute -top-7 md:-top-8 text-slate-550 text-sm md:text-base" htmlFor="height">Height</label>
+                <input 
+                    className="border border-[#e0e1e3] p-[22px] md:p-6 rounded-xl font-semibold leading-none text-[22px] w-full placeholder:text-[#C6C6C8]" 
+                    type="number" 
+                    name="height" 
+                    id="height" 
+                    value={heightFt}
+                    onChange={e => setHeightFt(e.target.value)} 
+                    placeholder="6"
+                />
+                <span className="absolute align-middle font-semibold inline-block leading-none right-6 text-blue-500 text-[23px] top-6 unit-height">ft</span>
+            </div>
+
+            <div className="inline-block mb-11 md:mb-0 relative w-full md:w-[24%]">
+                <label className="hidden" htmlFor="height">Height</label>
+                <input 
+                    className="border border-[#e0e1e3] p-[22px] md:p-6 rounded-xl font-semibold leading-none text-[22px] w-full placeholder:text-[#C6C6C8]" 
+                    type="number" 
+                    name="height" 
+                    id="height" 
+                    value={heightIn}
+                    onChange={e => setHeightIn(e.target.value)} 
+                    placeholder="2"
+                />
+                <span className="absolute align-middle font-semibold inline-block leading-none right-6 text-blue-500 text-[23px] top-6 unit-height">in</span>
+            </div>
+
+            <div className="inline-block relative w-full md:w-[24%]">
+                <label className="absolute -top-7 md:-top-8 text-slate-550 text-sm md:text-base" htmlFor="weight">Weight</label>
+                <input 
+                    className="border border-[#e0e1e3] p-[22px] md:p-6 rounded-xl font-semibold leading-none text-[22px] w-full placeholder:text-[#C6C6C8]" 
+                    type="number" 
+                    name="weight" 
+                    id="weight" 
+                    value={weightSt}
+                    onChange={e => setWeightSt(e.target.value)} 
+                    placeholder="12" 
+                />
+                <span className="absolute align-middle font-semibold inline-block leading-none right-6 text-blue-500 text-[23px] top-6 unit-weight">st</span>
+            </div>
+
+            <div className="inline-block relative w-full md:w-[24%]">
+                <label className="hidden" htmlFor="weight">Weight</label>
+                <input 
+                    className="border border-[#e0e1e3] p-[22px] md:p-6 rounded-xl font-semibold leading-none text-[22px] w-full placeholder:text-[#C6C6C8]" 
+                    type="number" 
+                    name="weight" 
+                    id="weight" 
+                    value={weightLbs}
+                    onChange={e => setWeightLbs(e.target.value)} 
+                    placeholder="9" 
+                />
+                <span className="absolute align-middle font-semibold inline-block leading-none right-6 text-blue-500 text-[23px] top-6 unit-weight">lbs</span>
+            </div>
+        </div>
+    )
 
     return (
         <form className="bg-white drop-shadow-2xl p-6 md:p-9 rounded-2xl" onSubmit={handleSubmit}>
@@ -65,99 +148,8 @@ export default function Form() {
                     </label>
                 </div>
             </div>
-
-            {/* Metric */}
-
-            <div className={`flex flex-wrap justify-between pt-6 ${metricClass}`}>
-                <div className="inline-block mb-11 md:mb-0 relative w-full md:w-[48%]">
-                    <label className="absolute -top-7 md:-top-8 text-slate-550 text-sm md:text-base" htmlFor="height">Height</label>
-                    <input 
-                        className="border border-[#e0e1e3] p-[22px] md:p-6 rounded-xl font-semibold leading-none text-[22px] w-full placeholder:text-[#C6C6C8]" 
-                        type="number" 
-                        name="height" 
-                        id="height" 
-                        value={height}
-                        onChange={e => setHeight(e.target.value)} 
-                        placeholder="0"
-                    />
-                    <span className="absolute align-middle font-semibold inline-block leading-none right-6 text-blue-500 text-[23px] top-6 unit-height">cm</span>
-                </div>
-
-                <div className="inline-block relative w-full md:w-[48%]">
-                    <label className="absolute -top-7 md:-top-8 text-slate-550 text-sm md:text-base" htmlFor="weight">Weight</label>
-                    <input 
-                        className="border border-[#e0e1e3] p-[22px] md:p-6 rounded-xl font-semibold leading-none text-[22px] w-full placeholder:text-[#C6C6C8]" 
-                        type="number" 
-                        name="weight" 
-                        id="weight" 
-                        value={weight}
-                        onChange={e => setWeight(e.target.value)} 
-                        placeholder="0" 
-                    />
-                    <span className="absolute align-middle font-semibold inline-block leading-none right-6 text-blue-500 text-[23px] top-6 unit-weight">kg</span>
-                </div>
-            </div>
-
-
-            {/* Imperial */}
-
-            <div className={`flex flex-wrap justify-between pt-6 ${imperialClass}`}>
-                <div className="inline-block mb-11 md:mb-0 relative w-full md:w-[24%]">
-                    <label className="absolute -top-7 md:-top-8 text-slate-550 text-sm md:text-base" htmlFor="height">Height</label>
-                    <input 
-                        className="border border-[#e0e1e3] p-[22px] md:p-6 rounded-xl font-semibold leading-none text-[22px] w-full placeholder:text-[#C6C6C8]" 
-                        type="number" 
-                        name="height" 
-                        id="height" 
-                        value={height}
-                        onChange={e => setHeight(e.target.value)} 
-                        placeholder="0"
-                    />
-                    <span className="absolute align-middle font-semibold inline-block leading-none right-6 text-blue-500 text-[23px] top-6 unit-height">ft</span>
-                </div>
-
-                <div className="inline-block mb-11 md:mb-0 relative w-full md:w-[24%]">
-                    <label className="hidden" htmlFor="height">Height</label>
-                    <input 
-                        className="border border-[#e0e1e3] p-[22px] md:p-6 rounded-xl font-semibold leading-none text-[22px] w-full placeholder:text-[#C6C6C8]" 
-                        type="number" 
-                        name="height" 
-                        id="height" 
-                        value={height}
-                        onChange={e => setHeight(e.target.value)} 
-                        placeholder="0"
-                    />
-                    <span className="absolute align-middle font-semibold inline-block leading-none right-6 text-blue-500 text-[23px] top-6 unit-height">in</span>
-                </div>
-
-                <div className="inline-block relative w-full md:w-[24%]">
-                    <label className="absolute -top-7 md:-top-8 text-slate-550 text-sm md:text-base" htmlFor="weight">Weight</label>
-                    <input 
-                        className="border border-[#e0e1e3] p-[22px] md:p-6 rounded-xl font-semibold leading-none text-[22px] w-full placeholder:text-[#C6C6C8]" 
-                        type="number" 
-                        name="weight" 
-                        id="weight" 
-                        value={weight}
-                        onChange={e => setWeight(e.target.value)} 
-                        placeholder="0" 
-                    />
-                    <span className="absolute align-middle font-semibold inline-block leading-none right-6 text-blue-500 text-[23px] top-6 unit-weight">st</span>
-                </div>
-
-                <div className="inline-block relative w-full md:w-[24%]">
-                    <label className="hidden" htmlFor="weight">Weight</label>
-                    <input 
-                        className="border border-[#e0e1e3] p-[22px] md:p-6 rounded-xl font-semibold leading-none text-[22px] w-full placeholder:text-[#C6C6C8]" 
-                        type="number" 
-                        name="weight" 
-                        id="weight" 
-                        value={weight}
-                        onChange={e => setWeight(e.target.value)} 
-                        placeholder="0" 
-                    />
-                    <span className="absolute align-middle font-semibold inline-block leading-none right-6 text-blue-500 text-[23px] top-6 unit-weight">lbs</span>
-                </div>
-            </div>
+            
+            {showImperial ? imperialFields : metricFields}
 
             <div className="mt-6">
                 <button className="border-2 border-slate-820 bg-slate-820 hover:bg-slate-800 hover:border-slate-800 font-medium px-6 py-4 rounded-xl text-white text-xl w-full" type="submit">Calculate</button>
--- src/functions/calculation.js
+++ src/functions/calculation.js
@@ -1,21 +1,30 @@
+import convertHeight from './convertHeight'
+import convertWeight from './convertWeight'
+
 export default function calculation(units, height, weight) {
-    /*
-    const heightMetric = height
-    const weightMetric = weight
-    const bmiCalculation = weightMetric/(heightMetric * heightMetric)
-    const bmi = Math.round(bmiCalculation * 10) / 10
-    */
 
-    let bmi = 0
+    // BMI calculation function - weight (in kg) divided by height in metres squared
+    const bmiCalculation = (height, weight) => weight / (height * height)
 
-    if (units === 'metric') {
-        let calc = weight / (height * height)
-        bmi = Math.round(calc * 10) / 10
-    } else if (units === 'imperial') {
-        // 
-    } else {
-        console.error('Invalid units of measure.')
+    let bmi = 0,
+    system = units ? 'imperial' : 'metric'
+
+    if (system === 'imperial') {
+        // Height parameter is expected to be an array containing 2 numbers - feet and inches
+        const metricHeight = convertHeight(height)
+
+        // Weight parameter is also expected to be an array containing 2 numbers - stone and pounds
+        const metricWeight = convertWeight(weight)
+
+        bmi = bmiCalculation(metricHeight, metricWeight)
+    } else if (system === 'metric') {
+        // Convert the height from centimetres to metres
+        const heightMetric = height / 100
+        const weightMetric = weight
+
+        bmi = bmiCalculation(heightMetric, weightMetric)
     }
     
-    return bmi
+    // Return the BMI rounded to 1 decimal place if not a whole number
+    return Math.round(bmi * 10) / 10
 }

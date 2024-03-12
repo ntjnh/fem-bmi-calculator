9747f295569e622c27a0e602768d1da0fbcd6634
Nate J.M <nate@natejonah.com>
2024-03-12 20:52:54 +0000

    Update limitation icon paths

--- src/data/limitations.js
+++ src/data/limitations.js
@@ -1,26 +1,26 @@
 export default [
     {
-        icon: '/src/assets/img/icon-gender.svg',
+        icon: '../assets/img/icon-gender.svg',
         heading: 'Gender',
         paragraph: 'The development and body fat composition of girls and boys vary with age. Consequently, a child’s age and gender are considered when evaluating their BMI.'
     },
     {
-        icon: '/src/assets/img/icon-age.svg',
+        icon: '../assets/img/icon-age.svg',
         heading: 'Age',
         paragraph: 'In aging individuals, increased body fat and muscle loss may cause BMI to underestimate body fat content.'
     },
     {
-        icon: '/src/assets/img/icon-muscle.svg',
+        icon: '../assets/img/icon-muscle.svg',
         heading: 'Muscle',
         paragraph: 'BMI may misclassify muscular individuals as overweight or obese, as it doesn’t differentiate muscle from fat.'
     },
     {
-        icon: '/src/assets/img/icon-pregnancy.svg',
+        icon: '../assets/img/icon-pregnancy.svg',
         heading: 'Pregnancy',
         paragraph: 'Expectant mothers experience weight gain due to their growing baby. Maintaining a healthy pre-pregnancy BMI is advisable to minimise health risks for both mother and child.'
     },
     {
-        icon: '/src/assets/img/icon-race.svg',
+        icon: '../assets/img/icon-race.svg',
         heading: 'Race',
         paragraph: 'Certain health concerns may affect individuals of some Black and Asian origins at lower BMIs than others. To learn more, it is advised to discuss this with your GP or practice nurse.'
     }
